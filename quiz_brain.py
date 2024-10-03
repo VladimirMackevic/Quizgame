86469fe73196f6c0a86a5d8aed5e28a86403c786
Admin <voldamarmc@gmail.com>
2024-10-03 21:57:00 +0200

    Initial commit

---
+++ quiz_brain.py
@@ -0,0 +1,10 @@
+
+class Answer:
+    def __init__(self, q_list):
+        self.q_num = 0
+        self.q_list = q_list
+
+    def next_question(self):
+        current_question = self.q_list[self.q_num]
+        self.q_num += 1
+        input(f'Q.{self.q_num}: {current_question.q_text} (True/False): ')
